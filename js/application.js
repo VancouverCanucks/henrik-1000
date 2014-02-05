31c68bdbb5adae0b6f4f6cd97b460eb5274945e8
Mike Timofiiv <mike.timofiiv@gmail.com>
2014-02-05 15:21:27 -0800

    fine-tuned the scroller

--- js/application.js
+++ js/application.js
@@ -5,7 +5,10 @@
   Scroller = (function() {
     
     function Scroller() {
+      window.scroller = this;
       this.initUI();
+      this.initSSOR();
+      this.scrollListener();
     }
     
     Scroller.prototype.initUI = function() {
@@ -13,6 +16,24 @@
         asset = $(this).children('.asset')[0];
         $(this).css('background-image', 'url(' + asset.src + ')').css('height', asset.height + 'px');
       });
+    }
+    
+    Scroller.prototype.initSSOR = function() {
+      this.ssor = $.superscrollorama();
+    }
+    
+    Scroller.prototype.scrollListener = function() {
+      $('.bookmark-marker').waypoint(function() {
+        window.scroller[this.getAttribute('callback')]();
+      });
+    }
+    
+    Scroller.prototype.toggleBookmark = function() {
+      if ($('.bookmark').hasClass('pin')) {
+        $('.bookmark').removeClass('pin').addClass('move');
+      } else {
+        $('.bookmark').removeClass('move').addClass('pin');
+      }
     }
     
     return Scroller;
