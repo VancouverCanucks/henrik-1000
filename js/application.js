fd0cc68e1fa9d594e77440d3c0b9e00235f2dbd1
Mike Timofiiv <mike.timofiiv@gmail.com>
2014-02-06 16:14:47 -0800

    props

--- js/application.js
+++ js/application.js
@@ -30,8 +30,10 @@
     }
     
     Scroller.prototype.scrollListener = function() {
-      $('.bookmark-marker').waypoint(function() {
-        window.scroller[this.getAttribute('callback')]();
+      $('.trigger').each(function() {
+        $(this).waypoint(function() {
+          window.scroller[this.getAttribute('callback')]();
+        });
       });
     }
     
@@ -41,6 +43,26 @@
       } else {
         $('.bookmark').removeClass('move').addClass('pin');
       }
+    }
+    
+    Scroller.prototype.toggleNewsClipping = function() {
+      $('#p01').fadeToggle();
+    }
+    
+    Scroller.prototype.toggleXO = function() {
+      $('#p02').fadeToggle();
+    }
+    
+    Scroller.prototype.toggleSkateIn = function() {
+      $('#p03').fadeToggle();
+    }
+    
+    Scroller.prototype.toggleA = function() {
+      $('#p04').fadeToggle();
+    }
+    
+    Scroller.prototype.toggleHats = function() {
+      $('#p05').fadeToggle();
     }
     
     return Scroller;
